c56c12ebc2d9fd50ac52d9c6ef445afc6e8c4b5a
thibaud <thibaudmarchand@hotmail.com>
2021-03-25 18:43:40 +0100

    push

--- controleur/controleur.js
+++ controleur/controleur.js
@@ -11,7 +11,7 @@ exports.getAll = (req,res) => {
 
 //recuperer une biere
 exports.getOne = (req,res) => {
-    Bieres.findOne({where: {id: 1}})
+    Bieres.findOne({where: {id: req.params.id}})
         .then(beer => res.status(200).json(beer)) //200 = OK, biere trouvee
         .catch(err => res.status(500).json(err)) //500 = erreur
 }
@@ -25,7 +25,7 @@ exports.getOne = (req,res) => {
             brewer: req.body.brewer,
             pays: req.body.pays
         }).then(() => {
-            res.status(200).json()
+            res.status(201).json()
         }).catch(err => res.status(500).json(err))
     }
 
@@ -37,25 +37,106 @@ exports.getOne = (req,res) => {
                 if (err) {
                     res.status(500).json(err) //500 = erreur
                 } else {
-                    res.status(400).json({error: 'Biere non trouvee'}) //400 = Biere non trouvee
+                    res.status(404).json({error: 'Biere non trouvee'}) //400 = Biere non trouvee
                 }
             })
     }
 
-// Modifier le nom d'une biere
-exports.updateName = (req,res) => {
+// Modifier la brasserie d'une biere (il n'y a pas de vérification pour regarder si le brewery_id correspond bien à brewer)
+exports.updateBrewery = (req,res) => {
     Bieres.findOne({where: {id: req.params.id}})
-        .then(res.status(200)) //200 = Ok biere supprimee
+        .then(biere => {
+            biere.brewery_id = req.body.brewery_id,
+            biere.brewer = req.body.brewer
+            biere.save().then(() => res.status(200)) //200 = Ok biere modifier
+        })
         .catch(err => {
             if (err) {
                 res.status(500).json(err) //500 = erreur
             } else {
-                res.status(400).json({error: 'Biere non trouvee'}) //400 = Biere non trouvee
+                res.status(404).json({error: 'Biere non trouvee'}) //400 = Biere non trouvee
             }
         })
 }
 
+// Modifier le nom d'une biere
+exports.updateName = (req,res) => {
+    Bieres.findOne({where: {id: req.params.id}})
+        .then(biere => {
+            biere.name = req.body.name
+            biere.save().then(() => res.status(200)) //200 = Ok biere modifier
+        })
+        .catch(err => {
+            if (err) {
+                res.status(500).json(err) //500 = erreur
+            } else {
+                res.status(404).json({error: 'Biere non trouvee'}) //400 = Biere non trouvee
+            }
+        })
+}
 
+// Modifier la brasserie d'une biere (il n'y a pas de vérification pour regarder si le brewery_id correspond bien à brewer)
+exports.updateBrewery = (req,res) => {
+    Bieres.findOne({where: {id: req.params.id}})
+        .then(biere => {
+            biere.brewery_id = req.body.brewery_id,
+                biere.brewer = req.body.brewer
+            biere.save().then(() => res.status(200)) //200 = Ok biere modifier
+        })
+        .catch(err => {
+            if (err) {
+                res.status(500).json(err) //500 = erreur
+            } else {
+                res.status(404).json({error: 'Biere non trouvee'}) //400 = Biere non trouvee
+            }
+        })
+}
 
+// Modifier l'abv d'une biere
+exports.updateABV = (req,res) => {
+    Bieres.findOne({where: {id: req.params.id}})
+        .then(biere => {
+            biere.abv = req.body.abv
+            biere.save().then(() => res.status(200)) //200 = Ok biere modifier
+        })
+        .catch(err => {
+            if (err) {
+                res.status(500).json(err) //500 = erreur
+            } else {
+                res.status(404).json({error: 'Biere non trouvee'}) //400 = Biere non trouvee
+            }
+        })
+}
 
+// Modifier la description d'une biere
+exports.updateDescription = (req,res) => {
+    Bieres.findOne({where: {id: req.params.id}})
+        .then(biere => {
+            biere.description = req.body.description
+            biere.save().then(() => res.status(200)) //200 = Ok biere modifier
+        })
+        .catch(err => {
+            if (err) {
+                res.status(500).json(err) //500 = erreur
+            } else {
+                res.status(404).json({error: 'Biere non trouvee'}) //400 = Biere non trouvee
+            }
+        })
+}
+
+// Modifier le pays d'origne d'une biere
+exports.updateDescription = (req,res) => {
+    Bieres.findOne({where: {id: req.params.id}})
+        .then(biere => {
+            biere.pays = req.body.pays
+            biere.save().then(() => res.status(200)) //200 = Ok biere modifier
+        })
+        .catch(err => {
+            if (err) {
+                res.status(500).json(err) //500 = erreur
+            } else {
+                res.status(404).json({error: 'Biere non trouvee'}) //400 = Biere non trouvee
+            }
+        })
+}
 
