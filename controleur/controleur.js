04a94811efdfec1f56d23320ae071603fe67ea90
thibaud <thibaudmarchand@hotmail.com>
2021-03-25 17:04:07 +0100

    push

--- controleur/controleur.js
+++ controleur/controleur.js
@@ -1,13 +1,27 @@
 const Bieres = require('../Models').Bieres;
 
 //Controleurs
+
+//récupérer toutes les bières
 exports.getAll = (req,res) => {
     Bieres.findAll()
         .then(all => res.status(200).json(all)) //200 = OK, all contient toutes les bieres
         .catch(err => res.status(500).json(err)) //500 = erreur
 }
 
-Bieres.create({
-    req
-})
+// Ajouter une bière
+exports.addBeer = (req, res) => {
+    Bieres.create({
+        brewery_id : req.body.brewery_id,
+        name: req.body.name,
+        abv: req.body.abv,
+        description: req.body.description,
+        brewer: req.body.brewer,
+        pays: req.body.pays
+    }).then(() => {
+        res.status(200).json()
+        }).catch(err => res.status(500).json(err))
+}
+
+
 
