1075a8ab393984198b8b9ad5292bc72d4d6cdbef
Romain GAUTIER <romain.gautier@etu.univ-nantes.fr>
2021-03-25 17:42:12 +0100

    test2

--- controleur/controleur.js
+++ controleur/controleur.js
@@ -9,19 +9,53 @@ exports.getAll = (req,res) => {
         .catch(err => res.status(500).json(err)) //500 = erreur
 }
 
-// Ajouter une bière
-exports.addBeer = (req, res) => {
-    Bieres.create({
-        brewery_id : req.body.brewery_id,
-        name: req.body.name,
-        abv: req.body.abv,
-        description: req.body.description,
-        brewer: req.body.brewer,
-        pays: req.body.pays
-    }).then(() => {
-        res.status(200).json()
-        }).catch(err => res.status(500).json(err))
+//recuperer une biere
+exports.getOne = (req,res) => {
+    Bieres.findOne({where: {id: 1}})
+        .then(beer => res.status(200).json(beer)) //200 = OK, biere trouvee
+        .catch(err => res.status(500).json(err)) //500 = erreur
 }
+// Ajouter une bière
+    exports.addBeer = (req, res) => {
+        Bieres.create({
+            brewery_id: req.body.brewery_id,
+            name: req.body.name,
+            abv: req.body.abv,
+            description: req.body.description,
+            brewer: req.body.brewer,
+            pays: req.body.pays
+        }).then(() => {
+            res.status(200).json()
+        }).catch(err => res.status(500).json(err))
+    }
+
+//supprimer une biere
+    exports.deleteBeer = (req, res) => {
+        Bieres.destroy({where: {id: req.params.id}})
+            .then(res.status(200)) //200 = Ok biere supprimee
+            .catch(err => {
+                if (err) {
+                    res.status(500).json(err) //500 = erreur
+                } else {
+                    res.status(400).json({error: 'Biere non trouvee'}) //400 = Biere non trouvee
+                }
+            })
+    }
+
+// Modifier le nom d'une biere
+exports.updateName = (req,res) => {
+    Bieres.findOne({where: {id: req.params.id}})
+        .then(res.status(200)) //200 = Ok biere supprimee
+        .catch(err => {
+            if (err) {
+                res.status(500).json(err) //500 = erreur
+            } else {
+                res.status(400).json({error: 'Biere non trouvee'}) //400 = Biere non trouvee
+            }
+        })
+}
+
+
 
 
 
--- routes/bieres.js
+++ routes/bieres.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var controleur = require('../controleur/controleur')
 
-router.get('/',controleur.getAll)
+router.get('/',controleur.getOne)
 
 module.exports = router;
+
